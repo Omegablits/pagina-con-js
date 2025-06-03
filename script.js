0cb6f23d726df66d271fe7bcf6c98b77a9ee4c26
Fernando <fmalleaantognoni@gmail.com>
2025-06-03 17:35:53 -0400

    botones inicio y registro diseñados

--- script.js
+++ script.js
@@ -1,6 +1,11 @@
-const btn = document.getElementById("btn");
 const container = document.querySelector(".container");
+const btnSignIn = document.getElementById("btn-sign-in");
+const btnSignUp = document.getElementById("btn-sign-up");
 
-btn.addEventListener("click",()=>{
-    container.classList.toggle("toggle");
+btnSignIn.addEventListener("click", ()=>{
+    container.classList.remove("toggle");
+});
+
+btnSignUp.addEventListener("click", ()=>{
+    container.classList.add("toggle");
 });
